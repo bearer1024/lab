226ca1e33e7f076e4a191207413fc1f6868f2aab
bearer1024 <bearer1024@gmail.com>
2016-11-02 19:07:38 +0000

    back up before batery run out

---
+++ reengineering/Assignment1/src/main/java/AssignmentSubmission.java
@@ -0,0 +1,150 @@
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+
+import util.DominanceTreeGenerator;
+import util.cfg.CFGExtractor;
+import util.cfg.Graph;
+import util.cfg.Node;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * This should be the entry-point of your programming submission.
+ *
+ * You may add as many of your own classes and packages as you like to this project structure.
+ *
+ */
+public class AssignmentSubmission implements Slicer {
+
+	//Set<Edge>
+	ClassNode targetClassNode;
+	MethodNode targetMethod;
+	Graph cfg;
+	
+	public AssignmentSubmission(String targetClass, String methodSignature){
+		targetClassNode = findClassNode(targetClass);
+		targetMethod = findMethodNode(targetClassNode, methodSignature);
+		try{
+			cfg = CFGExtractor.getCFG(targetClassNode.name, targetMethod);
+		} catch (AnalyzerException e) {
+			// Failed to extract CFG
+			e.printStackTrace();
+		}
+	}
+	
+	public static ClassNode findClassNode(String targetClass){
+		ClassNode target = new ClassNode(Opcodes.ASM4);
+        InputStream in=CFGExtractor.class.getResourceAsStream(targetClass);
+        ClassReader classReader;
+		try {
+			classReader = new ClassReader(in);
+			classReader.accept(target, 0);
+		} catch (IOException e1) {
+			// Fail to read class file.
+			e1.printStackTrace();
+		} 
+		return target;
+	}
+	
+	/**
+	 * Find the method with the given methodSignature, belonging to the targetClass.
+	 * @param targetClass
+	 * @param methodSignature
+	 * @return
+	 */
+	public static MethodNode findMethodNode(ClassNode targetClass, String methodSignature){
+		for(MethodNode mn : (List<MethodNode>)targetClass.methods){
+	        	String signature = mn.name+mn.desc;
+	        	if(!signature.equals(methodSignature))
+	        		continue;
+	        	else
+	        		return mn;
+		}
+		return null; //Method signature not found.
+	}
+
+    /**
+     * Returns true if there is a data dependence relation from a to b.
+     *
+     * In other words, a variable is assigned a value at a, and that value is
+     * subsequently used at b, without any intervening definitions of that variable.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    @Override
+    public boolean isDataDepence(AbstractInsnNode a, AbstractInsnNode b) {
+        //REPLACE THIS METHOD BODY WITH YOUR OWN CODE
+        return false;
+    }
+
+    /**
+     * Returns true if a is dependent upon b and false otherwise.
+     *
+     * In other words, returns true if b represents a conditional instruction that
+     * determines whether or not b will execute (following the definition of control
+     * dependence discussed in lectures).
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    @Override
+    public boolean isControlDependentUpon(AbstractInsnNode a, AbstractInsnNode b)  {
+    	
+    	//create augmented cfg
+    	Node firstNode = new Node(a);
+    	Node secondNode = new Node(b);
+    	Node startNode = new Node("Start"); 
+    	Node entryNode = cfg.getEntry();
+    	Node exitNode = cfg.getExit();
+    	
+    	cfg.addNode(startNode);
+    	cfg.addEdge(startNode,entryNode);
+    	cfg.addEdge(startNode,exitNode);
+
+    	//compute post-dominator tree
+        try {
+			DominanceTreeGenerator dominanceTreeGenerator = new DominanceTreeGenerator(cfg);
+			Graph postDominatorTreeGraph = dominanceTreeGenerator.postDominatorTree();
+
+
+			//compute control dependence graph
+			//if(postDominatorTreeGraph.
+		}
+		catch (AnalyzerException analyzerException){
+			analyzerException.printStackTrace();
+		}
+		catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+
+
+
+
+
+
+    	
+        return false;
+    }
+
+
+    /**
+     * Should return a backward slice on the criterion statement (for all variables).
+     * @param criterion
+     * @return
+     */
+    @Override
+    public List<AbstractInsnNode> backwardSlice(AbstractInsnNode criterion) {
+        //REPLACE THIS METHOD BODY WITH YOUR OWN CODE
+    	
+        return null;
+    }
+}
