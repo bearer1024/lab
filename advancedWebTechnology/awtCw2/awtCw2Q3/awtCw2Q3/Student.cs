488e59a840ebb87f45c3adb1dfd03a1b501e84ec
bearer1024 <bearer1024@gmail.com>
2016-12-04 11:05:19 +0000

    adding student class to the workflow

---
+++ advancedWebTechnology/awtCw2/awtCw2Q3/awtCw2Q3/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace awtCw2Q3
+{
+    public class Student
+    {
+        private string studentName;
+        private int studentNo;
+
+        public Student(string studentName,int studentNo)
+        {
+            this.StudentName = studentName;
+            this.StudentNo = studentNo;
+        }
+
+        public string StudentName
+        {
+            get
+            {
+                return studentName;
+            }
+
+            set
+            {
+                studentName = value;
+            }
+        }
+
+        public int StudentNo
+        {
+            get
+            {
+                return studentNo;
+            }
+
+            set
+            {
+                studentNo = value;
+            }
+        }
+    }
+   
+}
