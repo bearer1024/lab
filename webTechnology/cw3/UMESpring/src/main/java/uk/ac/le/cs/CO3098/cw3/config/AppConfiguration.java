8b1be2590c5eb4ede2f3081e9742e9b171355542
bearer1024 <bearer1024@gmail.com>
2016-12-25 00:33:20 +0000

    nice update for gradle from my lab9 v2

---
+++ webTechnology/cw3/UMESpring/src/main/java/uk/ac/le/cs/CO3098/cw3/config/AppConfiguration.java
@@ -0,0 +1,7 @@
+package uk.ac.le.cs.CO3098.cw3.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+}
