52ef291a5902e3144318711d68bb751ce720b21b
bearer1024 <bearer1024@gmail.com>
2016-12-26 11:07:43 +0000

    using umtclasscontroller now. plan to change it to hierarchyController

--- webTechnology/cw3/UMESpring/src/main/java/uk/ac/le/cs/CO3098/cw3/controller/UMTClassController.java
+++ webTechnology/cw3/UMESpring/src/main/java/uk/ac/le/cs/CO3098/cw3/controller/UMTClassController.java
@@ -6,20 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.ac.le.cs.CO3098.cw3.domain.UMTClass;
 import uk.ac.le.cs.CO3098.cw3.service.UMTClassService;
 
-@Controller
+@RestController
 @RequestMapping(value = {"/rest/class"})
 public class UMTClassController {
-@Autowired
-UMTClassService umtclassService;
+/*@Autowired
+UMTClassService umtclassService;*/
 
-@RequestMapping(value = {"/create"})
-	public ModelAndView CreateClass(Model model){
-	System.out.println(umtclassService);
-}
+@RequestMapping(value = {"/create"},method = RequestMethod.GET)
+	public @ResponseBody 
+	String getClassHierarchy(@RequestParam("className") String className){
+		System.out.println("className:"+className);
+		return new UMTClass(className).toString();
+	}
 }
